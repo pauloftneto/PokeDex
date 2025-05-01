3a3bfb8fae777947c7f25eb5c1b7b4fddcebd54f
pauloftneto <paulofneto28@gmail.com>
2025-04-30 23:25:59 -0300

    - Update namespace for `core-data` module to `br.ftdev.core.data`. - Update namespace for `core-domain` module to `br.ftdev.core.domain`. - Rename the test package in core-domain module from `br.ftdev.core_domain` to `br.ftdev.domain`. - Update the package name of the test file in the core-data module to match the new namespace. - Update `deploymentTargetSelector.xml` to include a new `SelectionState` for `LoadingIndicatorPreview`.

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "br.ftdev.core_ui"
+    namespace = "br.ftdev.core.ui"
     compileSdk = 35
 
     defaultConfig {
@@ -30,14 +31,44 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+// Kotlin Standard Library
+    implementation(libs.kotlin.stdlib)
 
+    // Dependências Principais do Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // AndroidX Core (necessário para algumas funcionalidades do Compose)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+
+    // Testes de UI com Compose
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debugging de UI com Compose
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
