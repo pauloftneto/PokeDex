81c46bb8d5db4764cca1c34d166ac7b6a73e0230
pauloftneto <paulofneto28@gmail.com>
2025-05-01 10:03:21 -0300

    - Introduce `feature-pokedex` module. - Add `FeaturePokedexModule` for dependency injection. - Implement `PokeDexViewModel` for presentation logic. - Configure module to use Jetpack Compose. - Include dependencies for Jetpack Compose, Koin, Coroutines, Coil, and others. - Add necessary build features and options for Compose. - Include the `core-domain` and `core-ui` modules as dependencies. - Add `kotlin-compose` plugin.

--- feature-pokedex/build.gradle.kts
+++ feature-pokedex/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -30,8 +31,51 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+    implementation(project(":core-domain"))
+    implementation(project(":core-ui"))
 
+    implementation(libs.coil.compose)
+
+    // Kotlin & Coroutines
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Dependências Principais do Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // Dependency Injection (Koin)
+    // Koin Core features
+    implementation(libs.koin.core)
+    // Koin Android features
+    implementation(libs.koin.android)
+    // Koin for Jetpack Compose
+    implementation(libs.koin.androidx.compose)
+
+    // Jetpack Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // AndroidX Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
 }
