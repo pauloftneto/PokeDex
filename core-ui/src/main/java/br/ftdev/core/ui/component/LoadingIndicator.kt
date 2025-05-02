fa290ef8caca2d2219f05003bc3249302d554d57
pauloftneto <paulofneto28@gmail.com>
2025-05-01 22:13:22 -0300

    - Add `isInitialLoading` parameter to `LoadingIndicator` to allow displaying either a `CircularProgressIndicator` for initial loading or a `LinearProgressIndicator` otherwise. - Update `LoadingIndicator` to display `LinearProgressIndicator` at the top when `isInitialLoading` is false. - Update `LoadingIndicator` to use a `CircularProgressIndicator` when `isInitialLoading` is true.

--- core-ui/src/main/java/br/ftdev/core/ui/component/LoadingIndicator.kt
+++ core-ui/src/main/java/br/ftdev/core/ui/component/LoadingIndicator.kt
@@ -2,7 +2,9 @@ package br.ftdev.core.ui.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -10,12 +12,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import br.ftdev.core.ui.theme.PokemonAppTheme
 
 @Composable
-fun LoadingIndicator(modifier: Modifier = Modifier) {
+fun LoadingIndicator(modifier: Modifier = Modifier, isInitialLoading: Boolean = false) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        CircularProgressIndicator()
+        if (isInitialLoading) {
+            CircularProgressIndicator()
+        } else {
+            LinearProgressIndicator(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .align(Alignment.TopCenter)
+            )
+        }
     }
 }
 
