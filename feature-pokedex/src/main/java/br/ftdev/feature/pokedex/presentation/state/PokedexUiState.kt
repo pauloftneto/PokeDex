7fbec178046f563a036fe0d5fb597a2b7496af28
pauloftneto <paulofneto28@gmail.com>
2025-05-01 22:13:47 -0300

    - Introduce `Loading` state with `isInitialLoading` flag in `PokedexUiState`.

--- feature-pokedex/src/main/java/br/ftdev/feature/pokedex/presentation/state/PokedexUiState.kt
+++ feature-pokedex/src/main/java/br/ftdev/feature/pokedex/presentation/state/PokedexUiState.kt
@@ -3,10 +3,14 @@ package br.ftdev.feature.pokedex.presentation.state
 import br.ftdev.core.domain.model.Pokemon
 
 sealed interface PokedexUiState {
-    data object Loading : PokedexUiState
+    data class Loading(
+        val isInitialLoading: Boolean = false
+    ) : PokedexUiState
+
     data class Success(
         val pokemonList: List<Pokemon>,
         val canLoadMore: Boolean
     ) : PokedexUiState
+
     data class Error(val message: String) : PokedexUiState
 }
