c93bcf2bc202841d8c82bb31bd591e756a3b5da4
pauloftneto <paulofneto28@gmail.com>
2025-05-02 19:32:44 -0300

    - Create `pokemonDetailsModule` for dependency injection. - Create `PokemonDetailsUiState` to manage UI state. - Create `PokemonDetailsViewModel` for handling Pokemon details logic. - Implement `fetchDetails` to get Pokemon details from `GetPokemonDetailsUseCase`. - Implement navigation for `pokemonId` using `SavedStateHandle`.

---
+++ feature-pokedex-details/src/main/java/br/ftdev/feature/pokedex/details/presentation/PokemonDetailsViewModel.kt
@@ -0,0 +1,45 @@
+package br.ftdev.feature.pokedex.details.presentation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.ftdev.core.domain.usecase.GetPokemonDetailsUseCase
+import br.ftdev.feature.pokedex.details.presentation.state.PokemonDetailsUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class PokemonDetailsViewModel(
+    private val getPokemonDetailsUseCase: GetPokemonDetailsUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<PokemonDetailsUiState>(PokemonDetailsUiState.Loading)
+    val uiState: StateFlow<PokemonDetailsUiState> = _uiState.asStateFlow()
+
+
+    private val pokemonId: Int = checkNotNull(savedStateHandle["pokemonId"]) {
+        "Pokemon ID não encontrado na navegação!"
+    }
+
+    init {
+        fetchDetails()
+    }
+
+    fun fetchDetails() {
+        _uiState.value = PokemonDetailsUiState.Loading
+        viewModelScope.launch {
+            getPokemonDetailsUseCase(pokemonId.toString())
+                .collect { result ->
+                    result.onSuccess { pokemon ->
+                        _uiState.value = PokemonDetailsUiState.Success(pokemon)
+                    }.onFailure { exception ->
+                        _uiState.value = PokemonDetailsUiState.Error(
+                            exception.message ?: "Erro ao carregar detalhes do Pokémon."
+                        )
+                    }
+                }
+        }
+    }
+}
